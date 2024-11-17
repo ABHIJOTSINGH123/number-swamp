00feb311ee41c2add9d34e924d0d976178d5011b
Abhijot singh <manjeetkaur2w00786@gmail.com>
2024-11-16 20:32:25 -0800

    python

---
+++ number swamp.py
@@ -0,0 +1,10 @@
+x=input("enter value of x")
+y=input("enter value of y")
+z=input("enter value of z")
+a=x
+x=y
+y=z
+z=a
+print("value of x after swamp",x)
+print("value of y after swamp",y)
+print("value of z after swamp",z)
